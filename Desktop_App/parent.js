a12de2f49f042b6fe8cb7516e62e50e5fa19b8da
Jeanneveev <147742822+Jeanneveev@users.noreply.github.com>
2025-01-21 12:04:23 -0500

    feat: Added basic page switching

---
+++ Desktop_App/parent.js
@@ -0,0 +1,26 @@
+function getQuestionNum(){
+    if(sessionStorage.getItem("questionNum")!==null){
+        console.log("exists");
+        return parseInt(sessionStorage.getItem("questionNum"));
+    }else{
+        console.log("Doesn't exist");
+        sessionStorage.setItem("questionNum","0");    //initializing questionNum for use in passing between pages
+        return 0;
+    }
+}
+function setQuestionNum(qVal){
+    sessionStorage.removeItem("questionNum");
+    sessionStorage.setItem("questionNum",String(qVal));
+}
+function pushToBackendList(input_value){
+    fetch(`${SERVER_URL}/appendToList/${input_value}`,{
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    }).then(response=>response.json()).then((data)=>{
+        console.log("You just entered:",data.value);
+    })
+}
+export { getQuestionNum, setQuestionNum, pushToBackendList };
---
+++ Desktop_App/py/routes.py
@@ -0,0 +1,28 @@
+from flask import Flask
+app = Flask(__name__)
+
+list_o_details:list[str]=[]
+list_o_sums:list[int]=[]
+
+@app.route("/", methods=["GET","POST"])
+def check():
+    return {"result":"Server active!"}
+
+@app.route("/add/<num1>/<num2>", methods=["POST"])
+def addNumbers(num1,num2):
+    num1=int(num1)
+    num2=int(num2)
+    sum = num1+num2
+    list_o_sums.append(sum)
+    return {"sum":f"{num1}+{num2}={sum}"}
+
+@app.route("/appendToList/<some_value>", methods=["POST"])
+def appendToList(some_value):
+    list_o_details.append(some_value)
+    print(f"Appended {some_value}")
+    return {"value":some_value}
+
+print(app.url_map)
+
+if __name__ == "__main__":
+    app.run(debug=True)
