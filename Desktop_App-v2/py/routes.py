4d4268ffd8209211778ce56bf689c2520a0e7ef6
Jeanneveev <147742822+Jeanneveev@users.noreply.github.com>
2025-02-02 16:46:04 -0500

    feat: Added functionality to add and view questions

--- Desktop_App-v2/py/routes.py
+++ Desktop_App-v2/py/routes.py
@@ -15,14 +15,27 @@ class Question:
         self.a_type=a_type
     def __str__(self)->str:
         return f"{self.q_detail}"
+    def as_dict(self)->dict:
+        return {"q_str":self.q_str,"q_detail":self.q_detail,
+                "q_type":self.q_type.value,"a_type":self.a_type.value}
 class Node:
     def __init__(self,question):
         self.question:Question=question
+        self.addon:Question=None
         self.answer:str=None
         self.next:Node=None
     def __str__(self)->str:
-        return f"The Node is for the question \"{self.question.q_str}\""
-
+        s=f"The Node is for the question \"{self.question.q_str}\""
+        if self.addon:
+            s+=f". This Node also has the addon question \"{self.addon.q_str}\""
+        return s
+    def as_dict(self)->dict:
+        res_dict={"question":self.question.as_dict()}
+        if self.addon:
+            res_dict["addon"]=self.addon.as_dict()
+        if self.answer:
+            res_dict["answer"]=self.answer
+        return res_dict
 class LinkedList:
     def __init__(self):
         self.head:Node=None
@@ -50,20 +63,63 @@ class LinkedList:
             curr=curr.next
         res+="null"
         return res
-    def getByQType(self,val:str):
+    def getByQType(self,val:str)->list[str]:
         found:list[str]=[]
         curr:Node=self.head
         while curr:
-            print("loop started")
+            # print("loop started")
             if curr.question.q_type==QTypeOptions(val):
-                print("matched")
+                # print("matched")
                 found.append(curr.question.q_detail)
             curr=curr.next
         return found
+    def getByDetail(self,val:str)->Node|None:
+        curr:Node=self.head
+        while curr:
+            if curr.question.q_detail==val:
+                print("matched")
+                return curr
+            curr=curr.next
+        return None
+    def getAll(self)->list[dict]:
+        res=[]
+        curr:Node=self.head
+        while curr:
+            res.append(curr.as_dict())
+            curr=curr.next
+        return res
+
+# Singleton metaclass
+# NOTE: Need to test in production, may cause errors
+class Singleton(type):
+    _instance=None
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance=super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instance
+class DetailList(metaclass=Singleton):
+    def __init__(self):
+        self.content:list[str]=[]
+    def __str__(self):
+        return f"{self.content}"
+    def add(self,value):
+        self.content.append(value)
+    def get(self):
+        return self.content
+
+
 
 # ROUTES
-from flask import Flask, request
+from flask import Flask, request, session
+# from flask_session import Session
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "YourSecretKey@123"
+app.config['SESSION_TYPE'] = 'filesystem'
+# app.config['SESSION_PERMANENT']= False
+
+# Session(app)
 ll=LinkedList() #initialize linked list to be used later
 
 #check that the server's running and connected
@@ -71,8 +127,6 @@ ll=LinkedList() #initialize linked list to be used later
 def check():
     return {"result":"Server active!"}
 
-##ERROR!!! The updated linked list doesn't seem to save outside of the function, and thus can't be accessed by other functions
-#TODO: Think I fixed the error, make sure to check tomorrow
 @app.route("/add_question", methods=["POST"])
 def add_question():
     """Make a new Question with the info passed"""
@@ -93,34 +147,98 @@ def add_question():
     new_node=Node(new_question)
     ll.append(new_node)
     ll.printLL()
-    return "works"
+    return {"response":"added question"}
+@app.route("/add_question/addon", methods=["POST"])
+def add_addon():
+    result=request.form
+    #make a question from the results
+    q_type=QTypeOptions(result["q_type_2"])
+    a_type=ATypeOptions(result["a_type"])
+    new_question=Question(result["question"],result["detail"],q_type,a_type)
+    #get the node of the question this one is adding onto
+    base_detail=result["addon_to"]
+    base_node=ll.getByDetail(base_detail)
+    #set the base's addon value to the addon Question
+    base_node.addon=new_question
+    print(f"Addon \"{new_question.q_detail}\" added to base node \"{base_node.question.q_detail}\"")
+    return {"response":"added addon question"}
 
-@app.route("/test_add_question",methods=["GET","POST"])
-def test_add_question():
-    q=QTypeOptions("base")
-    a=ATypeOptions("open-ended")
-    print(q,a)
-    new_question=Question("example question","example",q,a)
-    print(new_question)
-    new_node=Node(new_question)
-    print(new_node)
-    ll.append(new_node)
-    ll.printLL()
-    return "works"
+@app.route("/add_detail/<detail>",methods=["POST"])
+def add_detail_to_list(detail):
+    """Add a q_detail to a list of q_details"""
+    # if "detail_list" not in session:
+    #     session["detail_list"]=[]
+    # detail_list:list[str]=session["detail_list"]
+    # detail_list.append(detail)
+    # session["detail_list"]=detail_list
+    # return {"response":f"existing details are {session["detail_list"]}"}
+    detail_list=DetailList()
+    detail_list.add(detail)
+    return {"response":f"existing details are {detail_list}"}
+@app.route("/get_all_details",methods=["GET"])
+def get_all_details():
+    detail_list=DetailList()
+    details=detail_list.get()
+    return {"result":details}
 
-@app.route("/get_all", methods=["GET"])
-def get_all():
+
+@app.route("/print_all", methods=["GET"])
+def print_all():
     ll.printLL()
     return {"result":ll.returnLL()}
 
-@app.route("/get_all_base", methods=["GET"])
-def get_all_base():
+@app.route("/get_all_base_details", methods=["GET"])
+def get_all_base_details():
     """Get the details of all nodes with the q_type 'base'"""
     base_list=ll.getByQType("base")
     return {"result":base_list}
+
+@app.route("/get_ll_json", methods=["GET"])
+def all_to_json():
+    """Get every node in the linked list and return them as json"""
+    print(ll.getAll())
+    return {"result":ll.getAll()}
     
 
-print(app.url_map)
+## TEST FUNCTIONS
+@app.route("/test_add_singular",methods=["GET","POST"])
+def test_add_singular():
+    q=QTypeOptions("singular")
+    a=ATypeOptions("open-ended")
+    # print(q,a)
+    new_question=Question("example question","single example",q,a)
+    print(f"New singular question: {new_question}")
+    new_node=Node(new_question)
+    print(f"New node: {new_node}")
+    ll.append(new_node)
+    ll.printLL()
+    return "singular works"
+@app.route("/test_add_base",methods=["GET","POST"])
+def test_add_base():
+    q=QTypeOptions("base")
+    a=ATypeOptions("open-ended")
+    # print(q,a)
+    new_question=Question("example question","base example",q,a)
+    print(f"New base question: {new_question}")
+    new_node=Node(new_question)
+    print(f"New node: {new_node}")
+    ll.append(new_node)
+    ll.printLL()
+    return "base works"
+@app.route("/test_add_addon",methods=["GET","POST"])
+def test_add_addon():
+    q=QTypeOptions("add-on")
+    a=ATypeOptions("multiple-choice")
+    new_question=Question("addon question","ex detail",q,a)
+    print(f"New addon question: {new_question}")
+    base_detail="base example"
+    base_node=ll.getByDetail(base_detail)
+    base_node.addon=new_question
+    print(f"Addon \"{new_question.q_str}\" added to base node \"{base_node.question.q_detail}\"")
+    return "addon works"
+
+
+# print(app.url_map)
 
 if __name__ == "__main__":
     app.run(debug=True)
